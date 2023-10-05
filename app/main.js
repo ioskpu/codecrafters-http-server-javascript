7f054d35516b33da280aaffd806a53fe3524f4ab
Luis Rafael Corales Hurtado <lrcorales@gmail.com>
2023-10-05 08:56:33 -0500

    pass 3nd stage

--- app/main.js
+++ app/main.js
@@ -3,6 +3,16 @@ const net = require("net");
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
+
+const checkPath = (path, socket) => {
+    if (path === "/") {
+        socket.write("HTTP/1.1 200 OK\r\n\r\n");
+        console.log("buenisimo...");
+    } else {
+        socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+        console.log("oh no...");
+    }
+}
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
   socket.on("close", () => {

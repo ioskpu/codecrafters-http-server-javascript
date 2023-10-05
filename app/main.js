69cef0b8999daab39cfa28040ef669574a58fb07
Luis Rafael Corales Hurtado <lrcorales@gmail.com>
2023-10-05 15:53:31 -0500

    vamos al 5to/mod

--- app/main.js
+++ app/main.js
@@ -1,127 +1,50 @@
-// const net = require("net");
-
-// // You can use print statements as follows for debugging, they'll be visible when running tests.
-// console.log("Logs from your program will appear here!");
-
-
-// //const checkPath = (path, socket) => {
-// const loopOnPath = (path, socket) => {
-//     let temp = "";
-//     let word = "";
-//     let flag = 0;
-//     let counter = 0;
-
-//     if (path === "/") {
-//         socket.write("HTTP/1.1 200 OK\r\n\r\n");
-//         //console.log("buenisimo...");
-
-//     // } else {
-//     //     socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
-//     //     console.log("oh no...");
-//     return word;
-//     }
-//     for (let i = 0; i < path.length; i++) {
-
-//         if(path[i] === "/"){
-//             counter++;
-//         }
-        
-//         if(i === 0 && path[i] != "/"){
-//             socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
-//             return null;
-//         }
-
-//         if(i === path.length - 1 && flag === 0){
-//             socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
-//             return null;
-//         }
-
-//         if(i !== 0 && path[i] === "/" && counter === 2){
-//             if(temp.slice(1,) === "echo"){
-//                 flag = 1;
-//                 continue;
-//             } else {
-//                 socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
-//                 return null;                
-//             }
-//             }
-//             if(flag === 1){
-//                 word += path[i];
-//             }
-//             temp += path[i];
-//         }
-//         return word;
-//     }
-   
-//     const parsePath = (path ,socket) => {
-//         const str = loopOnPath(path,socket);
-        
-//         if (str && str!=="") {
-//             socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${str.length}\r\n\r\n${str}`)
-    
-//         }
-//     }
-// // Uncomment this to pass the first stage
-// const server = net.createServer((socket) => {
-//   socket.on("close", () => {
-//     socket.end();
-//     server.close();
-//   });
-
-//   socket.on("data", data => {
-//     socket.read();
-//     //checkPath(data.toString().split("\n")[0].split(" ")[1], socket);
-//     parsePath(data.toString().split("\r\n")[0].split(" ")[1] , socket);
-//     socket.end();
-//   })
-// });
-
-// server.listen(4221, "localhost");
-
 const net = require("net");
+function badRequest(socket) {
+    socket.write("HTTP/1.1 400 Bad Request\r\n\r\n");
+    socket.end();
+}
+const handleRequest = (socket) => {
+    return (data) => {
+        const req = data.toString();
+        //const startLine = req.split("\r\n")[0];
+        const reqParts = req.split("\r\n");
 
-console.log("Logs from your program will appear here!");
+        const startLine = reqParts[0];
+        if (!startLine) badRequest(socket);
+        const [_0, path, _1] = startLine.split(" ");
+        if (!path) badRequest(socket);
+        if (path === "/") {
+            socket.write("HTTP/1.1 200 OK \r\n\r\n");
+        } else if (path === "/user-agent") {
+            const userAgentHeader = reqParts.find((part) =>
+                part.includes("User-Agent"),
+            );
+            if (!userAgentHeader) {
+                return;
+            }
+            const userAgent = userAgentHeader.split(" ")[1];
+            socket.write(
+                `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`,
 
-const parseRequest = (request) => {
-  const lines = request.split("\r\n");
-  const [method, path] = lines[0].split(" ");
-  const headers = {};
-  for (let i = 1; i < lines.length; i++) {
-    const [key, value] = lines[i].split(": ");
-    headers[key] = value;
-  }
-  return { method, path, headers };
+            );
+        } else if (path.startsWith("/echo")) {
+            const randomString = path.slice(6);
+            socket.write(
+                `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length:${randomString.length}\r\n\r\n${randomString}\r\n`,
+            );
+        } else {
+            socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+        }
+        socket.end();
+    };
 };
-
+// Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-  socket.on("close", () => {
-    socket.end();
-    server.close();
-  });
-
-  socket.on("data", (data) => {
-    const request = data.toString();
-    const parsedRequest = parseRequest(request);
-
-    if (
-      parsedRequest.method === "GET" &&
-      (parsedRequest.path === "/" || parsedRequest.path === "/user-agent")
-    ) {
-      if (parsedRequest.path === "/user-agent") {
-        const userAgent = parsedRequest.headers["User-Agent"];
-        const response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`;
-        socket.write(response);
-      } else {
-        const response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 0\r\n\r\n";
-        socket.write(response);
-      }
-    } else {
-      const response = "HTTP/1.1 404 Not Found\r\n\r\n";
-      socket.write(response);
-    }
-
-    socket.end();
-  });
+    socket.on("close", () => {
+        socket.end();
+        server.close();
+    });
+    socket.on("data", handleRequest(socket));
 });
 
 server.listen(4221, "localhost");

cee04d95d9032f2ad4d931f76909a797c43d24bd
Luis Rafael Corales Hurtado <lrcorales@gmail.com>
2023-10-05 10:31:15 -0500

    pass 4nd stage/corregido

--- app/main.js
+++ app/main.js
@@ -4,16 +4,63 @@ const net = require("net");
 console.log("Logs from your program will appear here!");
 
 
-const checkPath = (path, socket) => {
+//const checkPath = (path, socket) => {
+const loopOnPath = (path, socket) => {
+    let temp = "";
+    let word = "";
+    let flag = 0;
+    let counter = 0;
+
     if (path === "/") {
         socket.write("HTTP/1.1 200 OK\r\n\r\n");
-        console.log("buenisimo...");
-    } else {
-        socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
-        console.log("oh no...");
-    }
-}
+        //console.log("buenisimo...");
 
+    // } else {
+    //     socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+    //     console.log("oh no...");
+    return word;
+    }
+    for (let i = 0; index < path.length; index++) {
+
+        if(path[i] === "/"){
+            counter++;
+        }
+        
+        if(i === 0 && path[i] != "/"){
+            socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+            return null;
+        }
+
+        if(i === path.length - 1 && flag === 0){
+            socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+            return null;
+        }
+
+        if(i! == 0 && path[i] === "/" && counter === 2){
+            if(temp.slice(1,) === "echo"){
+                flag = 1;
+                continue;
+            } else {
+                socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+                return null;                
+            }
+            }
+            if(flag === 1){
+                word += path[i];
+            }
+            temp += path[i];
+        }
+        return word;
+    }
+   
+    const parsePath = (path ,socket) => {
+        const str = loopOnPath(path,socket);
+        
+        if (str && str!=="") {
+            socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${str.length}\r\n\r\n${str}`)
+    
+        }
+    }
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
   socket.on("close", () => {
@@ -23,7 +70,8 @@ const server = net.createServer((socket) => {
 
   socket.on("data", data => {
     socket.read();
-    checkPath(data.toString().split("\n")[0].split(" ")[1], socket);
+    //checkPath(data.toString().split("\n")[0].split(" ")[1], socket);
+    parsePath(data.toString().split("\r\n")[0].split(" ")[1] , socket);
     socket.end();
   })
 });
